3e123aad0fbb9621402abf592fa1483f3f6e35c1
Antis <antis28@mail.ru>
2024-09-20 18:10:48 +0300

    Добавьте файлы проекта.

---
+++ CV19Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CV19Core.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19Core.ViewModels
+{
+    internal class MainViewModel : ViewModel
+    {
+        #region Title
+        private string _title = "Анализ статистики CV19";
+        /// <summary>Title wiwndow</summary>
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+        #endregion
+
+        /// <summary> Статус программы </summary>
+        private string _status = "Готов!";
+
+        /// <summary>
+        /// Статус программы
+        /// </summary>
+        public string Status
+        {
+            get { return _status; }
+            set { Set(ref _status, value); }
+        }
+
+    }
+}
