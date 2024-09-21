d99b90d9738c59b007f4e12b97c7e91694e91c42
Antis <antis28@mail.ru>
2024-09-21 15:17:47 +0300

    Извлечение данных по датам

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Net;
+﻿using System.Data;
+using System.Globalization;
+using System.Net;
 
 namespace CV19Console
 {
@@ -6,16 +8,49 @@ namespace CV19Console
     {
         private const string DataUrl =
             @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/refs/heads/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
-        private const string DataUrlActual = @"https://srhdpeuwpubsa.blob.core.windows.net/whdh/COVID/WHO-COVID-19-global-data.csv";
-       
+        //@"https://srhdpeuwpubsa.blob.core.windows.net/whdh/COVID/WHO-COVID-19-global-data.csv";
+
+
+        /// <summary>
+        /// Позволяет читать файл не скачивая его сразу весь.
+        /// </summary>
+        /// <returns>Поток для чтения файла</returns>
+        private static async Task<Stream> GetDataStream()
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(DataUrl, HttpCompletionOption.ResponseHeadersRead);
+            return await response.Content.ReadAsStreamAsync();
+        }
+
+        private static IEnumerable<string> GetDataLines()
+        {
+            using var dataStream = GetDataStream().Result;
+            using var dataReader = new StreamReader(dataStream);
+            while (!dataReader.EndOfStream)
+            {
+                var line = dataReader.ReadLine();
+                if (string.IsNullOrWhiteSpace(line)){continue;}
+                yield return line;
+            }
+        }
+
+        private static DateTime[] GetDates()=> GetDataLines()
+            .First()
+            .Split(',')
+            .Skip(4)
+            .Select(s=> DateTime.Parse(s, CultureInfo.InvariantCulture))
+            .ToArray();
+        
+
         static void Main(string[] args)
         {
-            //var client = new WebClient();
-            var client = new HttpClient();
+        //foreach (var dataLine in GetDataLines())
+        //{   
+        //    Console.WriteLine(dataLine);
+        //}
+            var dates = GetDates();
+            Console.WriteLine(string.Join("\r\n", dates));
 
-            var response = client.GetAsync(DataUrl).Result;
-            
-            var csvString = response.Content.ReadAsStringAsync().Result;
             Console.ReadLine();
         }
     }
