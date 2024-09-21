01cde73196df852e06865d2c1fe9d1d2e503dd40
Antis <antis28@mail.ru>
2024-09-21 09:02:20 +0300

    Получение сырых данных с сервера

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace CV19Console
+﻿using System.Net;
+
+namespace CV19Console
 {
     internal class Program
     {
+        private const string DataUrl =
+            @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/refs/heads/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+        private const string DataUrlActual = @"https://srhdpeuwpubsa.blob.core.windows.net/whdh/COVID/WHO-COVID-19-global-data.csv";
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //var client = new WebClient();
+            var client = new HttpClient();
+
+            var response = client.GetAsync(DataUrl).Result;
+            
+            var csvString = response.Content.ReadAsStringAsync().Result;
+            Console.ReadLine();
         }
     }
 }
