d5c0d26b07b46b1e7727e76837e9f30a9c3a5552
Antis <antis28@mail.ru>
2024-09-22 08:37:38 +0300

    Тестовая предметная область

---
+++ CV19Core/Models/Decanat/Student.cs
@@ -0,0 +1,18 @@
+﻿namespace CV19Core.Models.Decanat
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime Birthday { get; set; }
+        public double Rating { get; set; }
+
+    }
+
+    internal class Group
+    {
+        public string Name { get; set; }
+        public ICollection<Student> Students { get; set; }
+    }
+}
