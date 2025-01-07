54596264ca0b56ccc75b825f25c95e6d79a954fc
Antis <antis28@mail.ru>
2025-01-07 18:17:42 +0300

    Пример линейного конвертера

---
+++ CV19Core/Infrastructure/Converters/Linear.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CV19Core.Infrastructure.Converters.Base;
+
+namespace CV19Core.Infrastructure.Converters
+{
+    /// <summary>
+    /// f(x) y = k * x + b
+    /// </summary>
+    internal class Linear : Converter
+    {
+        public double K { get; set; } = 1;
+        public double B { get; set; }
+
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null) return null;
+
+            var x = System.Convert.ToDouble(value, culture);
+            return K * x + B;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null) return null;
+
+            var y = System.Convert.ToDouble(value, culture);
+            return (y - B) / K;
+        }
+    }
+}
